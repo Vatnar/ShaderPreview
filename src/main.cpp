ef6f2bb1707e5136f3d2352fd20605de1682d815
Peter Vatnar Andersson <Peter@70x7.net>
2025-04-08 13:55:17 +0200

    Added vcpkg.json and modified CMakeLists.txt to find packages

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <Windows.h>
+#include <d3d11.h>
+#include <imgui.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+#pragma comment(lib, "d3d11.lib")
+#pragma comment(lib, "dxgi.lib")
+#pragma comment(lib, "dxguid.lib")
+int main() {
+	return 0;
+}

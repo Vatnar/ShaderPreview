586ddabec1536045588c8b94f9fcce63e2df724b
Peter Vatnar Andersson <peter@70x7.net>
2025-06-07 17:04:03 +0200

    Fixing error handling

--- shader_preview/src/lib.rs
+++ shader_preview/src/lib.rs
@@ -1,52 +1,11 @@
 use crate::WindowError::InvalidSize;
 use vatnar_linalg::Vector2;
+pub mod error;
+
 // Window setup, event handling, imgui stuff
 
 // Linalg library
 // Rendering
-
-pub mod error {
-    use std::fmt::{Display, Formatter};
-
-    #[derive(Debug)]
-    pub enum Error {
-        WindowError(WindowError),
-    }
-    impl Display for Error {
-        fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-            match self {
-                Error::WindowError(inner) => write!(f, "Window Error: {} ", inner),
-            }
-        }
-    }
-    impl std::error::Error for Error {
-        fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
-            match self {
-                Error::WindowError(inner) => Some(inner),
-            }
-        }
-    }
-
-    #[derive(Debug)]
-    pub enum WindowError {
-        InvalidSize,
-    }
-
-    impl Display for WindowError {
-        fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-            match self {
-                WindowError::InvalidSize => write!(f, "Invalid size of window"),
-            }
-        }
-    }
-    impl std::error::Error for WindowError {}
-
-    impl From<WindowError> for Error {
-        fn from(err: WindowError) -> Self {
-            Error::WindowError(err)
-        }
-    }
-}
 
 use error::WindowError;
 pub fn create_window<T>(size: T) -> Result<(), WindowError>
--- shader_preview/src/main.rs
+++ shader_preview/src/main.rs
@@ -1,7 +1,15 @@
-use shader_preview::*;
+use shader_preview::create_window;
+use shader_preview::error::ShaderPreviewError;
 use vatnar_linalg::{Point2, Vector2};
 
-fn main() -> Result<(), error::Error> {
+fn main() {
+    if let Err(e) = run() {
+        eprintln!("Error: {}", e);
+        std::process::exit(1);
+    }
+}
+
+fn run() -> Result<(), ShaderPreviewError> {
     let size = Vector2::new(800, 600);
     create_window(size)?;
 
@@ -13,12 +21,12 @@ fn main() -> Result<(), error::Error> {
 
     origin += v * 3f64;
 
-    let screenpos: Point2<u32> = (300, 300).into();
+    let screen_pos: Point2<u32> = (600, 300).into();
 
-    let screenpos = screenpos.checked_sub((300, 400).into());
-    match screenpos {
-        Some(t) => println!("Valid op"),
-        None => println!("Out of bounds"),
+    if let Some(screen_pos) = screen_pos.checked_sub((300, 300).into()) {
+        println!("{screen_pos}")
+    } else {
+        return Err((ShaderPreviewError::out_of_bounds("Out of screen")));
     }
 
     Ok(())
